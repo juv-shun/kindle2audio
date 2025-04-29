6625c9152422ffcfcb14f25e294f8814554e4688
Shun Fukusumi <shun.fukusumi@gmail.com>
2025-04-29 12:17:50 +0900

    一時ディレクトリをtempfileライブラリで実現

--- synthesis.py
+++ synthesis.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import tempfile
 import wave
 from pathlib import Path
 
@@ -13,42 +14,39 @@ headers = {"Content-Type": "application/json"}
 
 
 def main():
-    # output/temp_wavディレクトリを作成
-    temp_dir = Path(__file__).with_name("output") / "temp_wav"
-    os.makedirs(temp_dir, exist_ok=True)
+    # 一時ディレクトリを作成
+    with tempfile.TemporaryDirectory() as td:
+        temp_dir = Path(td)
 
-    file_count = 1
-    for line in open(Path(__file__).with_name("output") / "voice_script.txt", "r", encoding="utf-8"):
-        # 空行はスキップ
-        line = line.strip()
-        if not line:
-            continue
+        file_count = 1
+        for line in open(Path(__file__).with_name("output") / "voice_script.txt", "r", encoding="utf-8"):
+            # 空行はスキップ
+            line = line.strip()
+            if not line:
+                continue
 
-        # /audio_query APIを呼び出してクエリを作成
-        response = requests.post(
-            f"{VOICEVOX_BASE_URL}/audio_query", headers=headers, params={"text": line, "speaker": SPEAKER_ID}
-        )
+            # /audio_query APIを呼び出してクエリを作成
+            response = requests.post(
+                f"{VOICEVOX_BASE_URL}/audio_query", headers=headers, params={"text": line, "speaker": SPEAKER_ID}
+            )
 
-        # /synthesis APIを呼び出して音声合成
-        response = requests.post(
-            f"{VOICEVOX_BASE_URL}/synthesis",
-            headers=headers,
-            params={"speaker": SPEAKER_ID},
-            json=response.json(),
-        )
+            # /synthesis APIを呼び出して音声合成
+            response = requests.post(
+                f"{VOICEVOX_BASE_URL}/synthesis",
+                headers=headers,
+                params={"speaker": SPEAKER_ID},
+                json=response.json(),
+            )
 
-        with open(temp_dir / f"voice_{file_count:03d}.wav", "wb") as wf:
-            wf.write(response.content)
+            with open(temp_dir / f"voice_{file_count:03d}.wav", "wb") as wf:
+                wf.write(response.content)
 
-        print(f"音声合成が完了しました。voice_{file_count:03d}.wavを保存しました。")
-        file_count += 1
+            print(f"音声合成が完了しました。voice_{file_count:03d}.wavを保存しました。")
+            file_count += 1
 
-    # temp_wavディレクトリの内容をoutput/voice.wavに結合
-    join_wav(sorted(temp_dir.glob("*.wav")), Path(__file__).with_name("output") / "voice.wav")
-    print("音声合成が完了しました。output/voice.wavを保存しました。")
-
-    # 一時ディレクトリを削除
-    shutil.rmtree(temp_dir)
+            # temp_wavディレクトリの内容をoutput/voice.wavに結合
+            join_wav(sorted(temp_dir.glob("*.wav")), Path(__file__).with_name("output") / "voice.wav")
+            print("音声合成が完了しました。output/voice.wavを保存しました。")
 
 
 def join_wav(inputs: list[Path], output: Path):
